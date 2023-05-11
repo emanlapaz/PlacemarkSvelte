8b4fc860bfde85634b4879b32525cca34ecdb2ac
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-05-12 06:28:45 +0800

    base donation.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
-<div class="notification is-dark">
-    Hello from Placemark!
-  </div>
-  
+<script>
+	import Header from '$lib/Header.svelte';
+	import WelcomeNavigator from '$lib/WelcomeNavigator.svelte';
+</script>
+
+<Header>
+	<WelcomeNavigator />
+</Header>
+
+<div class="columns is-vcentered content">
+	<div class="column has-text-centered">
+		<img width="300" src="/homer.png" alt="homer" />
+	</div>
+	<div class="column">
+		<h1 class="title">Help Me Run Springfield</h1>
+		<p>Donate what you can now - No Bitcoins accepted!</p>
+	</div>
+</div>
